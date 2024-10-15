1ba24fc60821ab8438a55bfbcae58fce37a82547
tokhirov700 <sarvartohirov133@gmail.com>
2024-10-15 08:08:18 +0500

    2 work

--- src/service/categories.ts
+++ src/service/categories.ts
@@ -1,11 +1,4 @@
-// import https from './config'
-// const category = {
-//     create: (data) => https.post("/category/create", data),
-//     get: (params) => https.get("/category/search", {params}),
-//     update: (id, data) => https.patch(`/category/update/${id}`, data),
-//     delete: (id) => https.delete(`/category/delete/${id}`),
-// };
-// export default category 
+
 import { CategoryValues } from '../components/types';
 import https from './config'
 const category = {
